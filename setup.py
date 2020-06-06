dd584fa98035550c3b337be7a67cd59838bffd60
MatthewTe <teelucksingh.matthew1@gmail.com>
2020-06-06 19:42:31 -0400

    Pushing setup.py for pip packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pdf_parser_api",
+    version="0.0.1",
+    author="MatthewTe",
+    author_email="teelucksingh.matthew1@gmail.com",
+    description="A package for converting a pdf to textual data and writing to a sqlite database",
+    long_description=long_description,
+    url="https://github.com/MatthewTe/pdf-parsing-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
