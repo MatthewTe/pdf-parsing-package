1ba039f07b71a459db480c3ba645fb53482d4702
MatthewTe <teelucksingh.matthew1@gmail.com>
2020-06-14 13:23:27 -0400

    Created Skeleton Framework for build_ticker_tbl method with Cosine Similarity

--- pdf_parsing_package/db_api.py
+++ pdf_parsing_package/db_api.py
@@ -1,13 +1,19 @@
 # Importing data management packages:
 from datetime import datetime
 import pandas as pd
+
 # Importing textual data cleaning packages:
 import string
 import re
+
+# Importing natural language packages:
 import nltk
 from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 # nltk.download('wordnet')
 # nltk.download('stopwords')
+
 # Importing the pdf api:
 # from . import pdf_parser as pparser # For Production
 import pdf_parser as pparser # For Development
@@ -38,11 +44,20 @@ class pdf_db(object):
         # Creating a singular cursor to interact with the database:
         self.c = self.con.cursor()
 
+        # Ensuring that the sqlite3 database supports foreign keys:
+        self.c.execute("PRAGMA foreign_keys")
+
+        # Enabling foreign key support if it is disabled:
+        if self.c.fetchall()[0][0] == 0:
+
+            self.c.execute("PRAGMA foreign_keys = ON")
+
         # Creating a summary table if it does not exist:
         self.c.execute(
             """CREATE TABLE IF NOT EXISTS Summary (
                     Name TEXT Primary Key,
                     Ticker TEXT,
+                    Pdf_type TEXT,
                     Date TEXT NOT NULL,
                     Path TEXT NOT NULL UNIQUE,
                     Date_added TEXT NOT NULL)"""
@@ -51,8 +66,9 @@ class pdf_db(object):
         # Commiting cursor command to database:
         self.con.commit()
 
+# <-----------------------------Database Writing Methods------------------------>
     # Method that writes a single pdf to the database:
-    def pdf_to_db(self, pdf_path, table_name, pdf_date, ticker):
+    def pdf_to_db(self, pdf_path, table_name, pdf_type, pdf_date, ticker):
         '''
         The method makes use of the pdf_parser api to write the generated key-value
         dict to the sqlite dictionary with the extracted strings cleaned for nlp
@@ -67,6 +83,9 @@ class pdf_db(object):
             A string that represents the title of the sqlite database that the data
             will be read into.
 
+        pdf_type : str
+            A string representing the category that the pdf is a part of. Eg: "10_K".
+
         pdf_date : str
             A string that indicates the date relevant to the pdf being uploaded
             to the database. This MUST be a full date in the form of dd/mm/yyyy.
@@ -76,12 +95,19 @@ class pdf_db(object):
             read to the database. This ticker will be written to the Summary table.
         '''
         # Creating the table:
+        # TODO: Update README with changes to the table schema once above change is made.
+
         self.c.execute(
             f"""CREATE TABLE IF NOT EXISTS {table_name} (
                 Section TEXT Primary Key,
                 Start_Page INTEGER,
                 End_Page INTEGER,
-                Section_Text TEXT)"""
+                Section_Text TEXT,
+                Cosine_Similarity REAL,
+                Jaccard_Similarity REAL,
+                Minimum_Edit_Distance REAL,
+                Simple_Similarity REAL
+                )"""
                     )
 
         # Initalzing the pdf parsing object:
@@ -128,9 +154,9 @@ class pdf_db(object):
 
         # Writing the logging/summary data to the summary database table:
         self.c.execute(
-            """INSERT INTO Summary VALUES (:name, :ticker, :date, :path, :date_written)""",
-                {'name': table_name, 'ticker': ticker, 'date': pdf_date, 'path': pdf_path,
-                'date_written': date_written}
+            """INSERT INTO Summary VALUES (:name, :ticker, :pdf_type, :date, :path, :date_written)""",
+                {'name': table_name, 'ticker': ticker, 'pdf_type':pdf_type,
+                 'date': pdf_date, 'path': pdf_path, 'date_written': date_written}
                 )
 
         # Commiting all changes to database:
@@ -155,8 +181,10 @@ class pdf_db(object):
         self.c.execute(
             f"""
             CREATE TABLE IF NOT EXISTS {table_name} (
-                Name TEXT UNIQUE,
-                date TEXT UNIQUE
+                Table_name TEXT UNIQUE,
+                Pdf_type TEXT,
+                Date TEXT UNIQUE,
+                FOREIGN KEY (Table_name) REFERENCES Summary (Name)
                 )""")
 
         self.con.commit()
@@ -170,16 +198,116 @@ class pdf_db(object):
         for tuple in self.c.fetchall():
 
             # Unpacking tuple:
-            (tbl_name, ticker_symbol, pdf_date, pdf_path, date_uploaded) = tuple
+            (tbl_name, ticker_symbol, pdf_type, pdf_date, pdf_path, date_uploaded) = tuple
 
             # Writing each row to the ticker database:
             self.c.execute(
                 f"""
-                INSERT OR IGNORE INTO {table_name} VALUES (:tbl_name, :pdf_date)
-                """, {'tbl_name': tbl_name, 'pdf_date':pdf_date})
+                INSERT OR IGNORE INTO {table_name} VALUES (:tbl_name, :pdf_type, :pdf_date)
+                """, {'tbl_name': tbl_name, 'pdf_type':pdf_type, 'pdf_date':pdf_date})
 
         self.con.commit()
 
+    # Method that executes natural language processing on all elements of a single ticker:
+    def perform_sim_calculation(self, ticker):
+        """
+        Method performs all elements of nlp similarity calculations between all
+        pdfs of a ceratin ticker symbol according to the lazy prices algorithm
+        described by the Documentation.
+
+        Parameters
+        ----------
+        ticker : str
+            A string that represents the ticker of pdfs that the method performs
+            similarity calculations on.
+        """
+        # Creating the main table name:
+        table_name = f'{ticker}_tables'
+
+        # Selecting the table based on the ticker symbol:
+        self.c.execute(f"SELECT * FROM {table_name}")
+        tuple_lst = self.c.fetchall()
+
+        # Iterating through the list of row tuples and performing sim operations on the respective table:
+        for tuple in tuple_lst:
+
+            # Attempting to extract previous year table name from fetchall:
+            prev_yr_tbl = pdf_db.build_tbl_name_tuple(tuple, tuple_lst)
+
+
+            # If there is a previous year continue else pass:
+            if prev_yr_tbl[1] != None:
+
+
+                # Building a list of Sections from primary pdf using list comprehension:
+                init_pdf_sections = [
+                    section_name[0] for section_name in self.c.execute(
+                    f"SELECT Section from {prev_yr_tbl[0]}")
+                    ]
+
+                # Builing a list of Sections form second pdf using list comprehension:
+                second_pdf_sections = [
+                section_name[0] for section_name in self.c.execute(
+                f"SELECT Section from {prev_yr_tbl[1]}")
+                ]
+
+                # Creating a list of only common pdf section names in init and second lst:
+                common_sections = list(set(init_pdf_sections).intersection(second_pdf_sections))
+
+                # print(prev_yr_tbl[0], len(init_pdf_sections), init_pdf_sections)
+                # print(prev_yr_tbl[1], len(second_pdf_sections), second_pdf_sections)
+                # print(prev_yr_tbl[0], len(common_sections), common_sections)
+                # print("-----------------------------------------------------------")
+
+                # Iterating through the common_sections, comparing corresponding text:
+                for section_name in common_sections:
+
+                    # Extracting all text data from the section of inital pdf:
+                    self.c.execute(
+                        f"""SELECT Section_Text FROM {prev_yr_tbl[0]}
+                        WHERE Section=:section_name""", {'section_name':section_name}
+                        )
+
+                    # self.c.fetchall() returns [(section_text, )]
+                    init_pdf_section_text = self.c.fetchall()[0][0]
+
+                    # Extracting all text data from the section of the second pdf:
+                    self.c.execute(
+                        f"""SELECT Section_Text from {prev_yr_tbl[1]} WHERE
+                        Section = :section_name""", {'section_name':section_name}
+                    )
+
+                    # Unpacking fetchall in same format as above:
+                    second_pdf_section_text = self.c.fetchall()[0][0]
+
+                    # Calculating the similarity between the init and second text:
+                    try: # If there are two strings available for calculations:
+
+                        # Calculating cosine similarity:
+                        cosine_sim = pdf_db.cosine_sim(init_pdf_section_text, second_pdf_section_text)
+
+                        # TODO: Perform Jaccard Similarity calculation:
+
+                        # TODO: Perform Minimum Edit Distance calculation:
+
+                        # TODO: Perform Simple Similarity calculation:
+
+                        # Writing the cosine similarity value to the inital pdf table:
+                        # TODO: Update execute statement to include all sim calculations:
+                        self.c.execute(
+                            f"""UPDATE {prev_yr_tbl[0]}
+                            SET Cosine_Similarity =:cosine_sim
+                            WHERE Section=:section_name""",
+                            {'cosine_sim':round(cosine_sim, 3), 'section_name':section_name})
+
+                    except:
+                        print("OOOF")
+
+
+            else:
+                pass
+
+# <------------------------------Query Methods---------------------------------->
     # Method that extracts an entire table of data:
     def get_table_data(self, table_name, section_title=None):
         '''
@@ -240,6 +368,8 @@ class pdf_db(object):
 
             return pdf_dict
 
+
+# <---------------------------'Helper' Methods----------------------------------->
     # Method that cleans the raw text string generated by the pdf_parser object:
     def clean_text(text):
         '''
@@ -270,7 +400,7 @@ class pdf_db(object):
 
         return clean_text
 
-    # Method that tokenizes and pre-processes string data when beinUNIQUEg extracted:
+    # Method that tokenizes and pre-processes string data when being extracted:
     def tokenize_text(text):
         '''
         This method is meant to be used as another 'helper' method in the
@@ -302,15 +432,107 @@ class pdf_db(object):
 
         return processed_txt
 
+    # Method that returns a tuple of a pdf table name and the pdf table name from
+    # the previous year given an input tuple and a list of tuples returned from ticker_tables:
+    def build_tbl_name_tuple(init_tuple, tuple_list):
+        '''
+        Method that iterates through a list of tuples of ('table_title', 'pdf_type', 'date')
+        given an inital tuple of the same format and determines the tuple that
+        corresponds to the previous year of the 'date' value of the init_tuple.
 
-# Class inherits from the pdf_db object to perform actions on the db to implement
-# the lazy prices algo:
-class lazy_prices(pdf_db):
-    """
-    This is the object that inherits the pdf_db object and contains all the methods
-    necessary for implementing the lazy prices algorithm. This includes creating
-    custom database tables, reading and writing data to tables and performing
-    nlp processing on data.
-    """
-    def __init__(self, path):
-        pass
+        See Docs for a more understandable explanation.
+
+        Parameters
+        ----------
+        init_tuple : tuple
+            A tuple extracted from a sqlite table query fetchall() list in the
+            format ('table_title','pdf_type','date').
+
+        tuple_list : lst
+            A list of tuples in the format ('table_title','pdf_type','date'). This
+            list is the list returned from the .fetchall() cursor method.
+
+        Returns
+        -------
+        name_tuple : tuple
+            A tuple in the format (init_tuple, previous_year_tuple) that is built
+            by the method. It contains the intial tuple and the tuple from the list
+            of tuples that is from the previous year and the same pdf category.
+        '''
+        # Unpacking init tuples:
+        (table_title, pdf_type, pdf_date) = init_tuple
+
+        # Converting the pdf date to a datetime object:
+        pdf_date = datetime.strptime(pdf_date, '%d/%m/%Y').date()
+
+        # Creating a list of tuples where the date values is the pervious year and same pdf_type
+        # via list comprehension: prev_tuple_lst SHOULD only contain one element.
+        prev_tuple_lst = [
+
+            # Converting 3rd element of tuple to a formatted datetime object:
+            (element[0], element[1], datetime.strptime(element[2], '%d/%m/%Y').date())
+            for element in tuple_list
+
+            # Only extracting tuple list where 3rd element date is previous year of pdf_date:
+            if datetime.strptime(element[2], '%d/%m/%Y').date().year == pdf_date.year - 1
+
+            # And the pdf_type (2nd element) is equal to the pdf_type of the pdf_type variable:
+            and element[1] == pdf_type
+            ]
+
+        # Building name_tuple out of the table names of the two relevant tuples:
+        try:
+            name_tuple = (table_title, prev_tuple_lst[0][0])
+
+        except:
+            # If no tuple is extracted via list comprehension, name_tuple[1] == None:
+            name_tuple = (table_title, None)
+
+        return name_tuple
+
+    # Method that computes the cosine similarity between two input strings:
+    def cosine_sim(str_1, str_2):
+        '''
+        This method computes the cosine similarity value between two input strings
+        via the use of the sklearn library.
+
+        Parameters
+        ----------
+        str_1 : This is the first string of text that will be used to compute
+            the cosine similarity.
+
+        str_2 : This is the second strign of text that will be used to compute the
+            cosine similarity.
+
+        Returns
+        -------
+        sim_value : float
+            A float representing the cosine similarity of the two strings calculated
+            by the cosine_similarity() method. This value is obtained by brute force
+            ripping the single value from the similarity matrix generated by the
+            above mentioned cosine_similarity method.
+        '''
+        # Creating a two element list of the input strings:
+        strings_lst = [str_1, str_2]
+
+        # Initalzing the TfidfVectorizer object:
+        tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+        tfidf_vectorizer = TfidfVectorizer()
+
+        # Converting strings_lst to a matrix of word counts:
+        sparse_matrix = tfidf_vectorizer.fit_transform(strings_lst)
+
+        # Computing the cosine similarity via a similarity matrix:
+        sim_matrix = cosine_similarity(sparse_matrix)
+
+        # Due to only two strings being compared, a matrix is not needed to express
+        # similarity values. Extracting similarity value via ripping matrix value:
+        sim_value = sim_matrix[0][1]
+
+        return sim_value
+
+    # TODO: Write a more complicated string matching method.
+
+test = pdf_db('/home/matthew/Documents/test_pdf_databases/test_db')
+#test.perform_sim_calculation('XOM')
+print(test.get_table_data('EXXON_10K_2019')['Cosine_Similarity'])
