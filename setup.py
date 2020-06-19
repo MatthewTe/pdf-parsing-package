7572639f1f6a40ab81bcb6c961e68d946037c485
MatthewTe <teelucksingh.matthew1@gmail.com>
2020-06-19 13:23:44 -0400

    Version Fix

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['PyPDF2', 'pdfplumber', 'pandas', 'nltk', 'textdistance', 'difflib']
+    install_requires=['PyPDF2', 'pdfplumber', 'pandas', 'nltk', 'textdistance']
 
 )
